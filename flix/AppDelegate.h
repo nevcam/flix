eeac3af9df2271eef2d830e53b7b7c862f0b2df9
Neval Cam <nev@fb.com>
2019-06-26 11:06:20 -0700

    Initial Commit

---
+++ flix/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  flix
+//
+//  Created by nev on 6/26/19.
+//  Copyright © 2019 nev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ flix/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  flix
+//
+//  Created by nev on 6/26/19.
+//  Copyright © 2019 nev. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
